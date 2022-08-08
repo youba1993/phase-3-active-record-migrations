56109c55ed7442237ddd6a56ed748ab262802ae5
youbaidjis <youba.etud@gmail.com>
2022-08-08 00:03:52 -0700

    commit tree d2a70d74146cd2ac3879bb9a824d9f566c637a09

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,20 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_relative "../app/models/artist"
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/artists.sqlite"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
